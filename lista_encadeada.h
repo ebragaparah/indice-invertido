427bd9575e25cd6b4e176cc53910637e56d23f0d
Eduardo Braga <eduardo@eduardo-macbook.local>
2011-04-20 19:54:06 -0300

    adicionada função de remoção de elemento tratando o primeiro elemento da lista encadeada

--- lista_encadeada.h
+++ lista_encadeada.h
@@ -38,5 +38,22 @@ void imprime_lista(tipo_palavra * lista) {
     for (; lista != NULL; lista = lista->prox) {
         printf("%s\r\n", lista->texto);
     }
+}
 
+int remove_elemento(tipo_palavra ** lista, char * texto){
+	tipo_palavra * lixo;
+	if((*lista)->texto == texto){
+		lixo = (*lista);
+		*lista = (*lista)->prox;
+		free(lixo);
+		return 1;
+	}
+	else{
+		while((*lista)->prox->texto != texto)
+			*lista = (*lista)->prox;
+		lixo = (*lista)->prox;
+		(*lista)->prox = (*lista)->prox->prox;
+		free(lixo);
+		return 1;
+	}
 }
