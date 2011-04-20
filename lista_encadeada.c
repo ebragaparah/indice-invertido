7bff9750a5eff877c088b295e5b308d97a41eb93
Douglas Camata <d.camata@gmail.com>
2011-04-20 19:23:30 -0300

    testes para lista encadeada adicionados.

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lista_encadeada.h"
 
 int main() {
+
+    tipo_palavra * palavra;
+
+    palavra = inicia_lista("oi");
+    insere_elemento(palavra, "tchau");
+    insere_elemento(palavra, "feliz natal");
+    imprime_lista(palavra);
 }
