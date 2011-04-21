2b4916b1664d2c4be8a7d99911218ca3bc959e1d
ThawanFidelis <thawan.fidelis@gmail.com>
2011-04-20 22:42:40 -0300

    adicionados mais testes da função de remoção da lista.

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "lista_encadeada.h"
 
+//Arquivo para testar as funções da biblioteca lista_encadeada.h
+
 int main() {
 
     tipo_palavra * palavra;
@@ -11,10 +13,26 @@ int main() {
     insere_elemento(palavra, "feliz natal");
     imprime_lista(palavra);
 	printf("-----\n");
+
 	remove_elemento(&palavra, "oi");
 	imprime_lista(palavra);
 	printf("-----\n");
+
+	remove_elemento(&palavra, "nao existe");
+	imprime_lista(palavra);
+	printf("-----\n");
+
 	remove_elemento(&palavra, "tchau");
 	imprime_lista(palavra);
 	printf("-----\n");
+
+	remove_elemento(&palavra, "nao existe");
+	imprime_lista(palavra);
+	printf("-----\n");
+
+	remove_elemento(&palavra, "feliz natal");
+	imprime_lista(palavra);
+	printf("-----\n");
+
 }
+
